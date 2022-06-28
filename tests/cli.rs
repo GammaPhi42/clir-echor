87486080a2c49152849370bef0dc800bee5e080e
Michael Miller <gammaphi42@gmail.com>
2022-06-28 06:11:52 -0600

    finished

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,33 +5,40 @@ use std::fs;
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
-}
-
-#[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
-}
-
-#[test]
-fn hello1() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
     Ok(())
 }
 
-fn hello2() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello2.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(vec!["Hello", "there"])
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
         .assert()
         .success()
         .stdout(expected);
     Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt") // two spaces between Hello and there
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
